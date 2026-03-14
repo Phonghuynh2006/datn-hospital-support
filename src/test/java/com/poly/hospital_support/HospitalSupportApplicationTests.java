a2ee508f871e438b1cf206a8cdf5d4e750e58a19
Phong Huynh <phonghuynh2006@gmail.com>
2026-03-14 08:30:56 +0700

    phonghuynh

---
+++ src/test/java/com/poly/hospital_support/HospitalSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.hospital_support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
